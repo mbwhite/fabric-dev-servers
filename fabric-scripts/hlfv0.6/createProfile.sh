9dd1ffb46f460cfc8dc108e5ffa6b9475efff2d2
Matthew B White <whitemat@uk.ibm.com>
2017-05-02 14:03:10 +0100

    update v0.6 profile creation

--- fabric-scripts/hlfv0.6/createProfile.sh
+++ fabric-scripts/hlfv0.6/createProfile.sh
@@ -3,3 +3,18 @@
 # Removing the existing profile for the Composer Connection Profiles
 rm -rf ~/.composer-connection-profiles/defaultProfile/*
 rm -rf ~/.composer-credentials/*
+
+# create a composer connection profile
+mkdir -p ~/.composer-connection-profiles/defaultProfile
+cat << EOF > ~/.composer-connection-profiles/defaultProfile/connection.json
+{
+    "type": "hlf",
+    "membershipServicesURL": "grpc://localhost:7054",
+    "peerURL": "grpc://localhost:7051",
+    "eventHubURL": "grpc://localhost:7053",
+    "keyValStore": "${HOME}/.composer-credentials",
+    "deployWaitTime": "300",
+    "invokeWaitTime": "100"
+
+}
+EOF
